0f785dcf9cedcdbcb28b09c527e8d6c4da902ac8
ronaldopires82 <99055150+ronaldopires82@users.noreply.github.com>
2022-02-16 09:20:24 -0300

    Aula 14 - Função print

---
+++ aula14.py
@@ -0,0 +1,15 @@
+# aula sobre string (texto) e aspas
+
+print("esta é uma 'string'")  # se quiser colocar aspas dentro de outras aspas,
+# pode combinar aspas duplas com aspas simples
+print('esta é uma "string"')  # ao contrário funciona também
+
+# caracteres de escape: a barra invertida (\) é utilizada como caractere de escape, mais ou menos assim:
+print('este é meu \'texto\'')  # isso é uma bosta, nem usa...
+
+# se usar por acaso \n, acaba fazendo uma quebra de linha na função print, assim:
+print('Meu nome é Ronaldo,\ne vou virar programador')
+
+# se quiser, dá pra colocar um r antes do primeiro argumento na função print pra não executar nada, mas só ler
+print(r'aqui não é para executar nada, mesmo que \n tenha uma quebra de linha')
+
